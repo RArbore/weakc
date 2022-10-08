06e7e9a2ad5c4d0df1d7c9536343435d25a78bce
Russel Arbore <russel.jma@gmail.com>
2022-10-08 11:14:42 -0500

    Incomplete lexer

--- parse/src/lex.rs
+++ parse/src/lex.rs
@@ -12,8 +12,9 @@
  * along with weakc. If not, see <https://www.gnu.org/licenses/>.
  */
 
-use super::combi::*;
+use super::combi;
 
+#[derive(Clone, Copy, Debug)]
 pub enum Token<'a> {
     Number(f64),
     String(&'a [u8]),
@@ -61,12 +62,59 @@ pub enum Token<'a> {
     Semicolon,
 }
 
-pub struct Lexer {}
+const SIMPLE_TOKENS: &[(&[u8], Token)] = &[
+    (b"F", Token::False),
+    (b"T", Token::True),
+    (b"p", Token::Print),
+    (b"a", Token::Variable),
+    (b"s", Token::Shape),
+    (b"sa", Token::ShapedAs),
+    (b"i", Token::If),
+    (b"w", Token::While),
+    (b"f", Token::Function),
+    (b"o", Token::Operator),
+    (b"r", Token::Return),
+    (b"N", Token::Nil),
+    (b"v", Token::Assert),
+    (b"+", Token::Plus),
+    (b"-", Token::Minus),
+    (b"*", Token::Star),
+    (b"/", Token::Slash),
+    (b"^", Token::Exp),
+    (b"@", Token::At),
+    (b"!", Token::Exclamation),
+    (b"=", Token::Equals),
+    (b">", Token::Greater),
+    (b"<", Token::Lesser),
+    (b"!=", Token::ExclamationEquals),
+    (b"==", Token::EqualsEquals),
+    (b">=", Token::GreaterEquals),
+    (b"<=", Token::LesserEquals),
+    (b"A", Token::And),
+    (b"O", Token::Or),
+    (b"(", Token::LeftParen),
+    (b")", Token::RightParen),
+    (b"{", Token::LeftBrace),
+    (b"}", Token::RightBrace),
+    (b"[", Token::LeftBracket),
+    (b"]", Token::RightBracket),
+    (b",", Token::Comma),
+    (b";", Token::Semicolon),
+];
 
-impl Lexer {
-    pub fn new() -> Lexer {
-        Lexer {}
+pub fn lex(chunk: &[u8]) -> Option<Vec<Token>> {
+    let mut tokens = vec![];
+    let mut current = chunk;
+
+    loop {
+        let attempt = combi::parse_any_of(current, SIMPLE_TOKENS);
+        if let Some((token, rest)) = attempt {
+            tokens.push(token);
+            current = rest;
+        } else {
+            break;
+        }
     }
 
-    pub fn lex(chunk: &[u8]) {}
+    Some(tokens)
 }
