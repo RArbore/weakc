5923efb4aac3cfab427a1020cc2f0b35592ad9e0
Russel Arbore <russel.jma@gmail.com>
2022-11-08 17:18:57 -0600

    Small amount of type checking, w/ test

--- semant/src/typecheck.rs
+++ semant/src/typecheck.rs
@@ -18,8 +18,9 @@ extern crate parse;
 use parse::ASTExpr;
 use parse::ASTStmt;
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Type {
+    Nil,
     Number,
     Tensor,
     Numeric,
@@ -39,23 +40,76 @@ pub fn typecheck<'a>(
     let mut types = bump.create_list();
     let mut symbols = vec![];
     for i in 0..ast.len() {
-        (types, symbols) = typecheck_stmt(ast.at(i), types, symbols, bump)?;
+        (types, symbols) = typecheck_stmt(ast.at(i), types, symbols)?;
     }
     Some(types)
 }
 
 fn typecheck_stmt<'a>(
     ast: &'a ASTStmt<'a>,
+    mut types: &'a mut bump::List<'a, Type>,
+    mut symbols: Vec<Symbol<'a>>,
+) -> Option<(&'a mut bump::List<'a, Type>, Vec<Symbol<'a>>)> {
+    match ast {
+        ASTStmt::Block(stmts) => {
+            let before_symbols = symbols.len();
+            for i in 0..stmts.len() {
+                (types, symbols) = typecheck_stmt(stmts.at(i), types, symbols)?;
+            }
+            symbols.truncate(before_symbols);
+        }
+        ASTStmt::Variable(var, init) => {
+            let (new_types, new_symbols, init_type) = typecheck_expr(init, types, symbols)?;
+            types = new_types;
+            symbols = new_symbols;
+            symbols.push(Symbol::Variable(var, init_type));
+        }
+        _ => panic!(),
+    }
+    Some((types, symbols))
+}
+
+fn typecheck_expr<'a>(
+    ast: &'a ASTExpr<'a>,
     types: &'a mut bump::List<'a, Type>,
     symbols: Vec<Symbol<'a>>,
-    bump: &'a bump::BumpAllocator,
-) -> Option<(&'a mut bump::List<'a, Type>, Vec<Symbol<'a>>)> {
-    Some((types, symbols))
+) -> Option<(&'a mut bump::List<'a, Type>, Vec<Symbol<'a>>, Type)> {
+    let my_type = match ast {
+        ASTExpr::Nil => Type::Nil,
+        ASTExpr::Identifier(var) => {
+            let mut found_type = None;
+            for symbol in symbols.iter() {
+                if let Symbol::Variable(potential_var, potential_type) = symbol {
+                    if var == potential_var {
+                        found_type = Some(*potential_type);
+                        break;
+                    }
+                }
+            }
+            found_type?
+        }
+        _ => panic!(),
+    };
+    types.push(my_type);
+    Some((types, symbols, my_type))
 }
 
 mod tests {
     use super::*;
 
     #[test]
-    fn typecheck1() {}
+    fn typecheck1() {
+        let bump = bump::BumpAllocator::new();
+        let ast = bump.create_list();
+        ast.push(ASTStmt::Variable(b"abc", bump.alloc(ASTExpr::Nil)));
+        ast.push(ASTStmt::Variable(
+            b"xyz",
+            bump.alloc(ASTExpr::Identifier(b"abc")),
+        ));
+        let typecheck = typecheck(ast, &bump).unwrap();
+        let correct_list = bump.create_list();
+        correct_list.push(Type::Nil);
+        correct_list.push(Type::Nil);
+        assert_eq!(typecheck, correct_list);
+    }
 }
