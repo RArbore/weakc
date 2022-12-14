80d52119378148b6aa422adc8bf26544a16174de
Russel Arbore <russel.jma@gmail.com>
2022-12-13 21:18:22 -0600

    Explicit IR ops

--- ir/src/ir.rs
+++ ir/src/ir.rs
@@ -25,22 +25,61 @@ pub enum IRType {
     Tensor,
 }
 
-type Register = (u32, IRType);
-type BasicBlockRef = u32;
+type IRRegister = (u32, IRType);
+type IRBasicBlockRef = u32;
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum IRUnaryOp {
+    Not,
+    Negate,
+    Shape,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum IRBinaryOp {
+    ShapedAs,
+    AddNumbers,
+    SubtractNumbers,
+    MultiplyNumbers,
+    DivideNumbers,
+    PowerNumbers,
+    AddTensors,
+    SubtractTensors,
+    MultiplyTensors,
+    DivideTensors,
+    PowerTensors,
+    MatrixMultiply,
+    Greater,
+    Lesser,
+    NotEqualsNils,
+    EqualsEqualsNils,
+    NotEqualsBooleans,
+    EqualsEqualsBooleans,
+    NotEqualsStrings,
+    EqualsEqualsStrings,
+    NotEqualsNumbers,
+    EqualsEqualsNumbers,
+    NotEqualsTensors,
+    EqualsEqualsTensors,
+    GreaterEquals,
+    LesserEquals,
+    And,
+    Or,
+}
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum IRInstruction<'a> {
-    Immediate(Register, semant::Value<'a>),
-    Copy(Register, Register),
-    Unary(Register, parse::ASTUnaryOp, Register),
-    Binary(Register, parse::ASTBinaryOp, Register, Register),
-    Index(Register, Register, &'a bump::List<'a, Register>),
-    BranchUncond(BasicBlockRef),
-    BranchCond(Register, BasicBlockRef, BasicBlockRef),
-    Call(Register, &'a [u8], &'a bump::List<'a, Register>),
-    Print(Register),
-    Verify(Register),
-    Return(Register),
+    Immediate(IRRegister, semant::Value<'a>),
+    Copy(IRRegister, IRRegister),
+    Unary(IRRegister, IRUnaryOp, IRRegister),
+    Binary(IRRegister, IRBinaryOp, IRRegister, IRRegister),
+    Index(IRRegister, IRRegister, &'a bump::List<'a, IRRegister>),
+    BranchUncond(IRBasicBlockRef),
+    BranchCond(IRRegister, IRBasicBlockRef, IRBasicBlockRef),
+    Call(IRRegister, &'a [u8], &'a bump::List<'a, IRRegister>),
+    Print(IRRegister),
+    Verify(IRRegister),
+    Return(IRRegister),
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -51,13 +90,13 @@ pub struct IRBasicBlock<'a> {
 #[derive(Debug, PartialEq, Clone)]
 pub struct IRFunction<'a> {
     name: &'a [u8],
-    params: &'a bump::List<'a, Register>,
+    params: &'a bump::List<'a, IRRegister>,
     ret_type: IRType,
     blocks: &'a bump::List<'a, IRBasicBlock<'a>>,
 }
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct IRModule<'a> {
-    globals: &'a bump::List<'a, Register>,
+    globals: &'a bump::List<'a, IRRegister>,
     funcs: &'a bump::List<'a, IRFunction<'a>>,
 }
