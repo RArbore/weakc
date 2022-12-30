5d220b9178cfc3b98bfacedb45b55fe95bdd6c8c
Russel Arbore <russel.jma@gmail.com>
2022-12-29 18:26:18 -0800

    Driver code

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -12,4 +12,151 @@
  * along with weakc. If not, see <https://www.gnu.org/licenses/>.
  */
 
-fn main() {}
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
+
+fn print_help() {
+    let text = r#"weakc: A compiler for the weak programming language.
+
+Usage:
+    weakc COMMAND [OPTIONS] INPUT
+
+Commands:
+    build         Compile the input program
+    run           Interpret the input program
+
+Options:
+    -h, --help            Display this message
+    -o, --output <OUTPUT> Set the output file when building
+"#;
+    println!("{}", text);
+}
+
+enum Command {
+    Build(String, String),
+    Run(String),
+}
+
+enum Option {
+    Output(String),
+}
+
+fn parse_options(args: &[String]) -> Result<(Vec<Option>, &[String]), String> {
+    let mut cursor = 0;
+    let mut options = vec![];
+    while cursor < args.len() {
+        match args[cursor].as_str() {
+            "-o" | "--output" => {
+                if cursor + 1 == args.len() {
+                    Err("ERROR: Used option that expected argument, but no argument provided.")?;
+                }
+                let output = args[cursor + 1].clone();
+                options.push(Option::Output(output));
+                cursor += 2;
+            }
+            _ => Err("ERROR: Unsupported option used.")?,
+        }
+    }
+    Ok((options, &args[cursor..]))
+}
+
+fn parse_command(args: &[String]) -> Result<(Command, &[String]), String> {
+    match args.get(0).ok_or("ERROR: No arguments provided.")?.as_str() {
+        "build" => {
+            let (options, args) = parse_options(&args[1..])?;
+            let input = args
+                .get(0)
+                .ok_or("ERROR: No input program provided.")?
+                .clone();
+            let mut output = if &input[input.len() - 5..] == ".weak" {
+                String::from(&input[input.len() - 5..]) + ".s"
+            } else {
+                input.clone() + ".s"
+            };
+            for option in options {
+                match option {
+                    Option::Output(out) => {
+                        output = out;
+                    }
+                }
+            }
+            let command = Command::Build(input, output);
+            Ok((command, &args[1..]))
+        }
+        "run" => {
+            let (options, args) = parse_options(&args[1..])?;
+            let input = args
+                .get(0)
+                .ok_or("ERROR: No input program provided.")?
+                .clone();
+            for option in options {
+                match option {
+                    Option::Output(_) => {
+                        Err("ERROR: Unsupported argument for command \"run\".")?;
+                    }
+                }
+            }
+            let command = Command::Run(input);
+            Ok((command, &args[1..]))
+        }
+        _ => Err("ERROR: Unsupported command used.")?,
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        0 => panic!("PANIC: Received 0 arguments from standard input."),
+        1 | 2 => {
+            println!("ERROR: Must provide at least 2 arguments. See usage below");
+            print_help();
+        }
+        _ => {
+            let command = parse_command(&args[1..]);
+            match command {
+                Ok((command, _)) => match command {
+                    Command::Build(input, output) => {
+                        let mut file =
+                            File::open(input).expect("PANIC: Unable to open input file.");
+                        let mut program = vec![];
+                        file.read_to_end(&mut program)
+                            .expect("PANIC: Unable to read input file.");
+                        let bump = bump::BumpAllocator::new();
+                        let tokens = parse::lex(&program, &bump)
+                            .expect("PANIC: Something went wrong during lexing.");
+                        let (ast, _) = parse::parse_program(&tokens, &bump)
+                            .expect("PANIC: Something went wrong during parsing.");
+                        let typed_program = semant::typecheck_program(ast, &bump)
+                            .expect("PANIC: Something went wrong during typechecking.");
+                        let ir_program = ir::irgen(typed_program, &bump);
+                        let mut file =
+                            File::open(output).expect("PANIC: Unable to open output file.");
+                        file.write_all(ir_program.to_string().as_bytes())
+                            .expect("PANIC: Unable to write output file.");
+                    }
+                    Command::Run(input) => {
+                        let mut file =
+                            File::open(input).expect("PANIC: Unable to open input file.");
+                        let mut program = vec![];
+                        file.read_to_end(&mut program)
+                            .expect("PANIC: Unable to read input file.");
+                        let bump = bump::BumpAllocator::new();
+                        let tokens = parse::lex(&program, &bump)
+                            .expect("PANIC: Something went wrong during lexing.");
+                        let (ast, _) = parse::parse_program(&tokens, &bump)
+                            .expect("PANIC: Something went wrong during parsing.");
+                        let context = semant::InterpContext::default();
+                        semant::eval_program(bump.alloc(ast), context)
+                            .expect("PANIC: Something went wrong during interpretation.");
+                    }
+                },
+                Err(error) => {
+                    println!("{}", error);
+                    print_help();
+                }
+            }
+        }
+    }
+}
