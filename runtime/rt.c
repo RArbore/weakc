098751cc2f094cb3da38f20ae7cd371c191a36c3
Russel Arbore <russel.jma@gmail.com>
2023-06-24 14:29:12 -0700

    progress on rt

--- runtime/rt.c
+++ runtime/rt.c
@@ -14,13 +14,16 @@
 
 #include <assert.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 #include <stdio.h>
+#include <math.h>
 
 typedef struct tensor {
-    unsigned num_dims;
-    unsigned *dim_sizes;
+    uint32_t num_dims;
+    uint32_t *dim_sizes;
     double *elements;
-} tensor;
+} tensor_t;
 
 void rt_memcpy(char *restrict dst, const char *restrict src, size_t size) {
     for (size_t i = 0; i < size; ++i) {
@@ -49,7 +52,7 @@ void rt_print_string(const char *val) {
     printf("%s\n", val);
 }
 
-void rt_print_number(double val) {
+void rt_print_number_sub(double val) {
     long precision = 1000000000;
 
     if (val < 0.0) {
@@ -71,23 +74,59 @@ void rt_print_number(double val) {
 	}
 	printf("%ld", fractional);
     }
+}
+
+void rt_print_number(double val) {
+    rt_print_number_sub(val);
     printf("\n");
 }
 
-void rt_print_tensor(const tensor *val) {
+void rt_print_tensor(const tensor_t *val) {
     printf("[");
     size_t total_size = 1;
     for (unsigned i = 0; i < val->num_dims; ++i) {
 	total_size *= val->dim_sizes[i];
     }
     for (size_t i = 0; i < total_size - 1; ++i) {
-	rt_print_number(val->elements[i]);
+	rt_print_number_sub(val->elements[i]);
 	printf(", ");
     }
-    rt_print_number(val->elements[total_size - 1]);
+    rt_print_number_sub(val->elements[total_size - 1]);
     printf("] sa [");
     for (unsigned i = 0; i < val->num_dims - 1; ++i) {
 	printf("%u, ", val->dim_sizes[i]);
     }
     printf("%u]\n", val->dim_sizes[val->num_dims - 1]);
+}
+
+void *rt_line() {
+    char *line = NULL;
+    size_t size;
+    ssize_t code = getline(&line, &size, stdin);
+    assert(code != -1);
+    return line;
+}
+
+double rt_power_reals(double a, double b) {
+    return pow(a, b);
+}
+
+tensor_t *rt_shaped_as(const tensor_t *a, const tensor_t *b) {
+    tensor_t *r = malloc(sizeof(tensor_t));
+    assert(b->num_dims == 1);
+    r->num_dims = b->dim_sizes[0];
+    r->dim_sizes = malloc(r->num_dims * sizeof(uint32_t));
+    size_t num_elements_b = 1;
+    for (uint32_t i = 0; i < r->num_dims; ++i) {
+	r->dim_sizes[i] = (uint32_t) b->elements[i];
+	num_elements_b *= r->dim_sizes[i];
+    }
+    size_t num_elements_a = 1;
+    for (uint32_t i = 0; i < a->num_dims; ++i) {
+	num_elements_b *= a->dim_sizes[i];
+    }
+    assert(num_elements_a == num_elements_b);
+    r->elements = malloc(num_elements_b * sizeof(double));
+    memcpy(r->elements, a->elements, num_elements_b * sizeof(double));
+    return r;
 }
